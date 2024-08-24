a29cec44704eb07e2e0837947956cef58da38907
Matheus Lopes <135535104+Lopes-Math@users.noreply.github.com>
2024-08-23 22:31:16 -0300

    Lista 01 - Exercício 22

--- Listas/Lista 01/exercicio22.cpp
+++ Listas/Lista 01/exercicio22.cpp
@@ -10,9 +10,9 @@
 //
 //                       Média de Aproveitamento Conceito
 //                          Entre 9.0 e 10.0	     A
-//                          Entre 7.5 e 9.0	         B
-//                          Entre 6.0 e 7.5	         C
-//                          Entre 4.0 e 6.0	         D
+//                          Entre 7.5 e 9.0	     B
+//                          Entre 6.0 e 7.5	     C
+//                          Entre 4.0 e 6.0	     D
 //                          Entre 4.0 e zero	     E
 //------------------------------------------------------------------------------
 
